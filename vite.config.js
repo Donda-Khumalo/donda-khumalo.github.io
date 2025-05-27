36caddd6610103d23351e0d45b258791a0ab35eb
Donda <the.donda.khumalo@gmail.com>
2025-05-28 01:30:27 +0300

    base change

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    base: '/donda-khumalo.github.io/', // 🔁 replace with your actual repo name!
+    base: './', // 🔁 replace with your actual repo name!
 })

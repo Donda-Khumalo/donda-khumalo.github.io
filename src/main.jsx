42fc0adcd0eb67da3ea3c485dcc37ff3e67202f2
Donda <the.donda.khumalo@gmail.com>
2025-06-04 05:08:28 +0300

    imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import {BrowserRouter, Link, Route, Routes, useLocation} from "react-router-dom";
+import App from "./App.jsx";
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(

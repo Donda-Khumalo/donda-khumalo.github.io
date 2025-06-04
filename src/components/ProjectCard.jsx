35eeeb9a7cefa0f0d80366cdc8c11afc7163522f
Donda <the.donda.khumalo@gmail.com>
2025-06-04 05:07:52 +0300

    improving cards

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 
-const ProjectCard = ({ title, description, link }) => (
-    <a href={link} target="_blank" rel="noopener noreferrer">
+
+const ProjectCard = ({title, description, location}) => (
+    <Link to={location}>
         <div className="project-card">
             <h3>{title}</h3>
             <p>{description}</p>

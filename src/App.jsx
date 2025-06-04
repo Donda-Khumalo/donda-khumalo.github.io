a4fd5740d967bfdb1cafeda590d5e9bcf6bd3515
Donda <the.donda.khumalo@gmail.com>
2025-06-04 05:08:09 +0300

    improving cards

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import Header from './components/Header';
 import ProjectSection from './components/ProjectSection.jsx';
 import { javaProjects, jupyterProjects, frontEndProjects } from './data/projects';
+import {BrowserRouter, Link, Route, Routes, useLocation} from "react-router-dom";
+import Projects from "./Projects";
+import HomePage from "./Homepage";
+import Details from "./Details";
+import './index.css';
 
 const App = () => {
     return (

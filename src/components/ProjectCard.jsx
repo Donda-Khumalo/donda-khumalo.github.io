3743e283df8ca1967990f2534262f2e689574521
Donda <the.donda.khumalo@gmail.com>
2025-06-04 05:07:20 +0300

    imports

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 const ProjectCard = ({ title, description, link }) => (
     <a href={link} target="_blank" rel="noopener noreferrer">
